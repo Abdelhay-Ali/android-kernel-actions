02da7cfb27c3bf5c7a706479098b2032cace6965
Abdelhay-Ali <118073201+Abdelhay-Ali@users.noreply.github.com>
2023-03-18 00:28:34 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -51,10 +51,7 @@ if [[ $arch = "arm64" ]]; then
         make_opts=""
         host_make_opts=""
 
-        if ! apt install -y --no-install-recommends gcc-"$ver_number" g++-"$ver_number" ; then
-            err "Compiler package not found, refer to the README for details"
-            exit 1
-        fi
+        apt install -y --no-install-recommends gcc-"$ver_number" g++-"$ver_number"
         wget -c https://releases.linaro.org/components/toolchain/binaries/7.5-2019.12/aarch64-elf/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-elf.tar.xz --no-check-certificate
         tar xf gcc-linaro-7.5.0-2019.12-x86_64_aarch64-elf.tar.xz
         
