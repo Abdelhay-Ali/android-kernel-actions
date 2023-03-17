14f3db38d99686f1691eb3f066ba7692e3e07b41
Abdelhay-Ali <118073201+Abdelhay-Ali@users.noreply.github.com>
2023-03-18 00:55:54 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -51,16 +51,18 @@ if [[ $arch = "arm64" ]]; then
         make_opts=""
         host_make_opts=""
 
-        apt install -y --no-install-recommends gcc-"$ver_number" g++-"$ver_number"
-        wget -c https://releases.linaro.org/components/toolchain/binaries/7.5-2019.12/aarch64-elf/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-elf.tar.xz --no-check-certificate
-        tar xf gcc-linaro-7.5.0-2019.12-x86_64_aarch64-elf.tar.xz
+        if ! apt install -y --no-install-recommends gcc-"$ver_number" g++-"$ver_number" \
+            gcc-"$ver_number"-aarch64-linux-gnu gcc-"$ver_number"-arm-linux-gnueabi; then
+            err "Compiler package not found, refer to the README for details"
+            exit 1
+        fi
         
         ln -sf /usr/bin/gcc-"$ver_number" /usr/bin/gcc
         ln -sf /usr/bin/g++-"$ver_number" /usr/bin/g++
         ln -sf /usr/bin/aarch64-linux-gnu-gcc-"$ver_number" /usr/bin/aarch64-linux-gnu-gcc
         ln -sf /usr/bin/arm-linux-gnueabi-gcc-"$ver_number" /usr/bin/arm-linux-gnueabi-gcc
 
-        export CROSS_COMPILE="/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-elf/bin/aarch64-elf-"
+        export CROSS_COMPILE="$workdir"/"$kernel_path""/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-elf/bin/aarch64-elf-"
         export CROSS_COMPILE_ARM32="arm-linux-gnueabi-"
     elif [[ $compiler = clang/* ]]; then
         ver="${compiler/clang\/}"
