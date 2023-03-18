bd324aa86c9eee4bc91d138e8ba5e933b445a2d8
Abdelhay-Ali <118073201+Abdelhay-Ali@users.noreply.github.com>
2023-03-18 14:14:02 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -207,7 +207,7 @@ tag="$(git branch | sed 's/*\ //g')"
 echo "branch/tag: $tag"
 echo "make options:" $arch_opts $make_opts $host_make_opts
 msg "Generating defconfig from \`make $defconfig\`..."
-if ! sudo make O=out $arch_opts $make_opts $host_make_opts "$defconfig"; then
+if ! make O=out $arch_opts $make_opts $host_make_opts "$defconfig"; then
     err "Failed generating .config, make sure it is actually available in arch/${arch}/configs/ and is a valid defconfig file"
     exit 2
 fi
